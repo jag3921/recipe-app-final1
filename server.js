3f63b89eedb76ace391f2011b09597830a737f81
jag3921 <jag3921@rit.edu>
2021-12-05 17:22:26 -0500

    m

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+const unirest = require("unirest");
+const API_KEY = "ee58002a-d577-452a-b2d2-e9b1d7fc4626";
+
+app.get('/api/associations/:word', (req, res) => {
+	const word = req.params.word;
+	const request = unirest.get(`https://api.wordassociations.net/associations/v1.0/json/search?apikey=${API_KEY}&lang=en&text=${word}`)
+	.then(response => {
+		const results = response.body.response[0].items || []; // grab array of results
+		console.log(`Num results=${results.length}`);
+		res.json(results);
+	})
+	.catch(error => {
+		console.log(`error=${error}`);
+		res.json({status:"Error", message: `${error}`});
+	});
+});
+
+app.listen(port, () => {
+  console.log(`word-app listening on port ${port}`);
+});
