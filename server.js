c354af92823cd258752e17f33f70b9b4564a5187
jag3921 <jag3921@rit.edu>
2021-12-05 19:20:30 -0500

    committed client folder

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 const unirest = require("unirest");
+const path = require('path');
 const API_KEY = "ee58002a-d577-452a-b2d2-e9b1d7fc4626";
 
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('/api/associations/:word', (req, res) => {
 	const word = req.params.word;
 	const request = unirest.get(`https://api.wordassociations.net/associations/v1.0/json/search?apikey=${API_KEY}&lang=en&text=${word}`)
@@ -18,6 +21,9 @@ app.get('/api/associations/:word', (req, res) => {
 	});
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
 app.listen(port, () => {
   console.log(`word-app listening on port ${port}`);
 });
